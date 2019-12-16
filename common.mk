e25c2d20ad1e2edcb2126779625e35c1e4db42d1
NurKeinNeid <mralexman3000@gmail.com>
2019-12-16 21:26:44 +0100

    Revert "msm8998-common: Enable OEM unlock and bootable image signing"
    This reverts commit a8f9d219614378bd1c3f1981a931c21d1ba7c1d3.
    
    Change-Id: I03ef7a4677dbb9f5f12e0f072a441e8065bd4443
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- common.mk
+++ common.mk
@@ -334,10 +334,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0-service \
     SecureElement
 
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -460,10 +456,6 @@ PRODUCT_PACKAGES += \
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2019-08-01
-
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
